dd2cfe5fd6f81e525a9bcab47d9e481e959951cd
joeljdv <joeldiazjdv@gmail.com>
2021-03-27 17:13:13 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit",addList)
 });
+
+
+function addList(event){
+    event.preventDefault()
+    const input = event.target.querySelector("#new-task-description").value
+    const liTag = document.createElement('li')
+    const task = document.querySelector("#tasks")
+    liTag.textContent = input
+    task.appendChild(liTag)
+    event.target.querySelector("#new-task-description").value = ""
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox"
+    checkbox.value = 1
+    checkbox.name = "done"
+    liTag.appendChild(checkbox)
+}
+
+
+
+
